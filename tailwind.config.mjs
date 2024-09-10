08de48eb367b83f48c7019fa0d9e5116a22759d9
Aaron <aaronvendedor@gmail.com>
2024-09-09 22:07:21 -0400

    Added hero section

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+			"background": "#FFFFFF",
+			"foreground": "#1E1E1E",
+			"text": "#1E1E1E",
+			"text-foreground": "#FFFFFF",
+			"primary": "#656565",
+			"card": "#1E1E1E/4",
+			"card-foreground": "#FFFFFF/4",
+			},
+		},
+	},
+	plugins: [],
+}
