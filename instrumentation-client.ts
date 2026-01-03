acf3596e34157417e4af6321b8fcd7ef225aef37
Aaron Avila <aaronvendedor@gmail.com>
2026-01-02 23:08:54 -0400

    fix: disabled compression

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,6 +3,7 @@ import posthog from "posthog-js";
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
   api_host: "/ingest",
   ui_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+  disable_compression: true,
   defaults: "2025-05-24",
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
